fe0b36324280e94202955c68e1bbb26ed17a2285
rawaqasha <r.qasha@newcastle.ac.uk>
2015-03-25 19:46:59 +0000

    all in one container

--- scripts/block-deploy2.sh
+++ scripts/block-deploy2.sh
@@ -12,19 +12,5 @@ set -e
 
 ctx logger info "Deploying ${BLOCk_NAME} on ${CONTAINER_ID}"
 
-if [[ -z ${GIT} ]]; then
-
-  sudo docker exec -it ${CONTAINER_ID} apt-get update
-  sudo docker exec -it ${CONTAINER_ID} apt-get -y install git
-
-else
-  ctx logger info "git already has been installed"
-fi
-
-
- sudo docker exec -it ${CONTAINER_ID} git clone ${BLOCK_URL}
-
-
-ctx logger info "Execute the block"
 sudo docker exec -it ${CONTAINER_ID} java -jar eSc-blocks/${BLOCK_NAME}
 
