f89ceaf7b7d98ce30a35166a6c51b3165e77e7b7
rawa <rawa@rawa-VirtualBox.(none)>
2015-03-25 12:27:27 +0000

    rpq full stack blueprint

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-CONTAINER_NAME=$(ctx node properties container_ID)
+CONTAINER_ID=$(ctx node properties container_ID)
 IMAGE_NAME=$(ctx node properties image_name)
+BLOCK_NAME=$(ctx node properties block_name)
 
-ctx logger info "Creating ${CONTAINER_NAME}"
+ctx logger info "Creating ${CONTAINER_ID}"
 
-sudo docker run --name ${CONTAINER_NAME} -it -d ${IMAGE_NAME} bin/bash
+sudo docker run --name ${CONTAINER_ID} -it -d ${IMAGE_NAME} java -jar eSc-blocks/${BLOCK_NAME}
+
