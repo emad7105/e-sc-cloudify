5ae342a8ce238152a9d39bb2b5dcdb9de4f77e77
rawaqasha <r.qasha@newcastle.ac.uk>
2015-05-07 18:42:53 +0100

    some modification

--- scripts/java-install2.sh
+++ scripts/java-install2.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-
+CONTAINER_ID=$1
 LIBRARY_NAME=$(ctx node properties lib_name)
 
 ctx logger info "Installing java on"
@@ -13,8 +13,8 @@ set -e
 
 if [[ -z ${java} ]]; then      
         
-sudo docker exec -it ${container_name} apt-get update
-sudo docker exec -it ${container_name} apt-get -y install ${LIBRARY_NAME}
+sudo docker exec -it ${CONTAINER_ID} apt-get update
+sudo docker exec -it ${CONTAINER_ID} apt-get -y install ${LIBRARY_NAME}
 
 else
   ctx logger info "Java already has been installed"
