7fcade030d0709ff089b70f48583ca150a1e3e06
rawaqasha <r.qasha@newcastle.ac.uk>
2015-07-27 13:53:14 +0100

    tidy up

---
+++ eGenome-workflow/scripts/megaCC-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+CONTAINER_NAME=$1
+Lib_URL=$2
+#LIBRARY_NAME=$(ctx node properties lib_name)
+
+#ctx logger info "Installing MegaCC lib on ${CONTAINER_NAME}"
+
+set +e
+  Git=$(sudo docker exec -it ${CONTAINER_ID} which git)
+set -e
+
+if [[ -z ${Git} ]]; then      
+        
+  sudo docker exec -it ${CONTAINER_NAME} apt-get update
+  sudo docker exec -it ${CONTAINER_NAME} apt-get -y install git
+
+fi
+
+sudo docker exec -it ${CONTAINER_NAME} git clone ${Lib_URL}
+
