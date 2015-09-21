6bdb264dba235582f702c0779191f427bb44c140
rawaqasha <r.qasha@newcastle.ac.uk>
2015-09-21 15:43:38 +0100

    types

--- Picard/scripts/get-Dir.sh
+++ Picard/scripts/get-Dir.sh
@@ -2,12 +2,12 @@
 
 set -e
 blueprint=$1
-Dir=~/myDir #$(ctx node properties SourceFolder)
+Dir=$(ctx node properties SourceFolder)
 
 sourceDir=${HOME}/${blueprint}/$(basename "$Dir")
 
 
-#ctx logger info "copy the Dir"
+ctx logger info "copy the Dir"
 
 
 cp -r ${Dir} ${sourceDir}
